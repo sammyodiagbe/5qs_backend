af582b7ad26fa2fb252623c1be3b9a21df828091
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-12 17:10:44 -0500

    configuration

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.send("working like a charm 😎");
+});
+
 app.listen(PORT, () => {
   console.log("listening on port ", PORT);
 });
